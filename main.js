ea2fb551305426216030b618f9cebcf24e170838
toasted-coder <christmanholmes21@gmail.com>
2021-01-18 16:50:28 -0800

    changed some file names, added a reset.css sheet, added to main JS file, and added to the style css file

---
+++ main.js
@@ -0,0 +1,115 @@
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.querySelector("button.submitBtn")
+var secondsLeft = (questions.length * 20 + 1);
+var timerElement = document.getElementById("timer");
+var submitScoreElement = document.querySelector("#submit-score");
+var userScoreElement = document.getElementById("user-score");
+var userNameInput;
+var questionHead = document.getElementById("questions");
+var answerChoices = document.getElementById("answers");
+
+var questionNumber = -1;
+var answer;
+
+
+function startTimer() {
+ 
+    document.getElementById("home").classList.add('d-none');
+    document.getElementById("quiz").classList.remove('d-none');
+
+    // timer set and begins 120 second countdown
+    setTimer();
+
+    // create questions to display
+    makeQuestions();
+}
+
+function setTimer() {
+
+    var countdown = setInterval(function () {
+        secondsLeft--;
+        timerElement.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0 || questionNumber === questions.length) {
+            clearInterval(countdown);
+            setTimeout(displayScore, 500);
+        }
+    }, 1000);
+}
+
+function makeQuestions() {
+    questionNumber++;
+    answer = questions[questionNumber].answer
+
+    questionHead.textContent = questions[questionNumber].title;
+    answerChoices.innerHTML = "";
+
+    var choices = questions[questionNumber].choices;
+
+    for (var q = 0; q < choices.length; q++) {
+        var nextChoice = document.createElement("button");
+
+        nextChoice.textContent = choices[q]
+        answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    }
+}
+
+// displays add name to score board
+function displayScore() {
+    document.getElementById("quiz").classList.add('d-none');
+    document.getElementById("submit-score").classList.remove('d-none');
+    userScoreElement.textContent = "FINAL SCORE: " + secondsLeft + ".";
+}
+
+// Event Listeners for main buttons
+startBtn.addEventListener("click", startTimer);
+submitBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    addScore();
+    
+    window.location.href = './highscores.html'
+});
+
+function addScore () {
+    userNameInput = document.getElementById("userName").value
+    
+    // creates a new object with name and score value
+var newScore = {
+        name: userNameInput,
+        score: secondsLeft
+    };
+    // checks localStorage for saved scores, if none creates new array
+    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+    // pushes player score into newly created array
+    highScores.push(newScore)
+    // turn objects into strings array and saves in localStorage
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function hideFeedback(){
+    var pElement = document.getElementsByClassName("feedback")[0]
+    pElement.style.display='none'
+}
+
+function showFeedback(){
+    var pElement = document.getElementsByClassName("feedback")[0]
+    pElement.removeAttribute('style');
+}
+
+answerChoices.addEventListener("click", function (event) {
+    var pElement = document.getElementsByClassName("feedback")[0]
+    
+    // displays if answer is correct or incorrect
+    if (answer === event.target.textContent) {   
+        pElement.innerHTML = "CORRECT";
+        setTimeout(hideFeedback,1225);
+        showFeedback();   
+        
+    } else {
+        pElement.innerHTML = "INCORRECT";
+        setTimeout(hideFeedback,1225);
+        secondsLeft = secondsLeft - 20;
+        showFeedback();
+    }    
+    makeQuestions();
+});
